8f454b017e04a7bf60f2a898f2789ce4dc28c72f
Michael Friedman <contact@michaeldavidfriedman.com>
2016-11-19 10:26:33 -0800

    Linted according to regular  non "standard" conventions.

--- js/index.js
+++ js/index.js
@@ -1,50 +1,83 @@
 (() => {
-  'use strict'
-  const colors = document.getElementsByTagName('span')
-  const divs = document.getElementsByTagName('div')
-  let currentColor = null
-  const colorIndicator = document.getElementById('colorIndicator')
-  let customColor = document.getElementById('customColor').value
-  const input = document.getElementById('customColor')
-  const namedColors = ['LightSalmon', 'Salmon', 'DarkSalmon', 'LightCoral', 'IndianRed', 'Crimson', 'FireBrick', 'DarkRed', 'Red', 'Pink', 'LightPink', 'HotPink', 'DeepPink', 'PaleVioletRed', 'MediumVioletRed', 'OrangeRed', 'Tomato', 'Coral', 'DarkOrange', 'Orange', 'Yellow', 'LightYellow', 'LemonChiffon', 'LightGoldenrodYellow', 'PapayaWhip', 'Moccasin', 'PeachPuff', 'PaleGoldenrod', 'Khaki', 'DarkKhaki', 'Gold', 'Cornsilk', 'BlanchedAlmond', 'Bisque', 'NavajoWhite', 'Wheat', 'BurlyWood', 'Tan', 'RosyBrown', 'SandyBrown', 'Goldenrod', 'DarkGoldenrod', 'Peru', 'Chocolate', 'SaddleBrown', 'Sienna', 'Brown', 'Maroon', 'DarkOliveGreen', 'Olive', 'OliveDrab', 'YellowGreen', 'LimeGreen', 'Lime', 'LawnGreen', 'Chartreuse', 'GreenYellow', 'SpringGreen', 'MediumSpringGreen', 'LightGreen', 'PaleGreen', 'DarkSeaGreen', 'MediumAquaMarine', 'MediumSeaGreen', 'SeaGreen', 'ForestGreen', 'Green', 'DarkGreen', 'Aqua', 'Cyan', 'LightCyan', 'PaleTurquoise', 'AquaMarine', 'Turquoise', 'MediumTurquoise', 'DarkTurquoise', 'LightSeaGreen', 'CadetBlue', 'DarkCyan', 'Teal', 'LightSteelBlue', 'PowderBlue', 'LightBlue', 'SkyBlue', 'LightSkyBlue', 'DeepSkyBlue', 'DodgerBlue', 'CornflowerBlue', 'Steelblue', 'RoyalBlue', 'Blue', 'MediumBlue', 'DarkBlue', 'Navy', 'MidnightBlue', 'Lavender', 'Thistle', 'Plum', 'Violet', 'Orchid', 'Fuchsia', 'Magenta', 'MediumOrchid', 'MediumPurple', 'BlueViolet', 'DarkViolet', 'DarkOrchid', 'DarkMagenta', 'Purple', 'Indigo', 'DarkSlateBlue', 'SlateBlue', 'MediumSlateBlue', 'White', 'Snow', 'Honeydew', 'MintCream', 'Azure', 'AliceBlue', 'GhostWhite', 'WhiteSmoke', 'Seashell', 'Beige', 'OldLace', 'FloralWhite', 'Ivory', 'AntiqueWhite', 'Linen', 'LavenderBlush', 'MistyRose', 'Gainsboro', 'LightGray', 'Silver', 'DarkGray', 'Gray', 'DimGray', 'LightSlateGray', 'DarkSlateGray', 'Black']
-  const colorIndicatorMessage = document.getElementById('colorIndicatorMessage')
+  'use strict';
+  const colors = document.getElementsByTagName('span');
+  const divs = document.getElementsByTagName('div');
+  let currentColor = null;
+  const colorIndicator = document.getElementById('colorIndicator');
+  let customColor = document.getElementById('customColor').value;
+  const input = document.getElementById('customColor');
+  const namedColors = ['LightSalmon', 'Salmon', 'DarkSalmon', 'LightCoral',
+    'IndianRed', 'Crimson', 'FireBrick', 'DarkRed', 'Red', 'Pink', 'LightPink',
+    'HotPink', 'DeepPink', 'PaleVioletRed', 'MediumVioletRed', 'OrangeRed',
+    'Tomato', 'Coral', 'DarkOrange', 'Orange', 'Yellow', 'LightYellow',
+    'LemonChiffon', 'LightGoldenrodYellow', 'PapayaWhip', 'Moccasin',
+    'PeachPuff', 'PaleGoldenrod', 'Khaki', 'DarkKhaki', 'Gold', 'Cornsilk',
+    'BlanchedAlmond', 'Bisque', 'NavajoWhite', 'Wheat', 'BurlyWood', 'Tan',
+    'RosyBrown', 'SandyBrown', 'Goldenrod', 'DarkGoldenrod', 'Peru',
+    'Chocolate', 'SaddleBrown', 'Sienna', 'Brown', 'Maroon', 'DarkOliveGreen',
+    'Olive', 'OliveDrab', 'YellowGreen', 'LimeGreen', 'Lime', 'LawnGreen',
+    'Chartreuse', 'GreenYellow', 'SpringGreen', 'MediumSpringGreen',
+    'LightGreen', 'PaleGreen', 'DarkSeaGreen', 'MediumAquaMarine',
+    'MediumSeaGreen', 'SeaGreen', 'ForestGreen', 'Green',
+    'DarkGreen', 'Aqua', 'Cyan', 'LightCyan', 'PaleTurquoise',
+    'AquaMarine', 'Turquoise', 'MediumTurquoise', 'DarkTurquoise',
+    'LightSeaGreen', 'CadetBlue', 'DarkCyan', 'Teal', 'LightSteelBlue',
+    'PowderBlue', 'LightBlue', 'SkyBlue', 'LightSkyBlue', 'DeepSkyBlue',
+    'DodgerBlue', 'CornflowerBlue', 'Steelblue', 'RoyalBlue', 'Blue',
+    'MediumBlue', 'DarkBlue', 'Navy', 'MidnightBlue', 'Lavender', 'Thistle',
+    'Plum', 'Violet', 'Orchid', 'Fuchsia', 'Magenta', 'MediumOrchid',
+    'MediumPurple', 'BlueViolet', 'DarkViolet', 'DarkOrchid',
+    'DarkMagenta', 'Purple', 'Indigo', 'DarkSlateBlue', 'SlateBlue',
+    'MediumSlateBlue', 'White', 'Snow', 'Honeydew', 'MintCream', 'Azure',
+    'AliceBlue', 'GhostWhite', 'WhiteSmoke', 'Seashell', 'Beige', 'OldLace',
+    'FloralWhite', 'Ivory', 'AntiqueWhite', 'Linen', 'LavenderBlush',
+    'MistyRose', 'Gainsboro', 'LightGray', 'Silver', 'DarkGray', 'Gray',
+    'DimGray', 'LightSlateGray', 'DarkSlateGray', 'Black'];
+
+  const colorIndicatorMessage =
+  document.getElementById('colorIndicatorMessage');
   const brushIndicator = () => {
-    colorIndicator.style.backgroundColor = currentColor
-    colorIndicatorMessage.textContent = `Brush Indicator: ${currentColor}`
-  }
+    colorIndicator.style.backgroundColor = currentColor;
+    colorIndicatorMessage.textContent = `Brush Indicator: ${currentColor}`;
+  };
+
   const createPallette = (arr) => {
     for (const namedColor of namedColors) {
-      const swatch = document.createElement('span')
-      document.getElementById('pallette').appendChild(swatch)
-      swatch.style.backgroundColor = namedColor
+      const swatch = document.createElement('span');
+
+      document.getElementById('pallette').appendChild(swatch);
+      swatch.style.backgroundColor = namedColor;
       if (swatch.style.backgroundColor === 'black') {
-        swatch.style.border = '1px solid white'
+        swatch.style.border = '1px solid white';
       }
     }
-  }
-  createPallette()
+  };
+
+  createPallette();
   const createGrid = () => {
     for (let i = 0; i < 1749; i++) {
-      const pixel = document.createElement('div')
-      document.getElementById('pixel-grid').appendChild(pixel)
+      const pixel = document.createElement('div');
+
+      document.getElementById('pixel-grid').appendChild(pixel);
     }
-  }
-  createGrid()
+  };
+
+  createGrid();
   input.addEventListener('input', () => {
-    customColor = document.getElementById('customColor').value
-    currentColor = customColor
-    brushIndicator()
-  })
+    customColor = document.getElementById('customColor').value;
+    currentColor = customColor;
+    brushIndicator();
+  });
   for (const color of colors) {
     color.addEventListener('click', () => {
-      currentColor = color.style.backgroundColor
-      colorIndicator.style.backgroundColor = currentColor
-      brushIndicator()
-    })
+      currentColor = color.style.backgroundColor;
+      colorIndicator.style.backgroundColor = currentColor;
+      brushIndicator();
+    });
   }
   for (const div of divs) {
     div.addEventListener('click', () => {
-      div.style.backgroundColor = currentColor
-    })
+      div.style.backgroundColor = currentColor;
+    });
   }
-})()
+})();
